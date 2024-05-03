de15151401a16cbd2c32d8610f47e6ae844b5ef5
Laman Salimova <104246181+LemanSelimova@users.noreply.github.com>
2024-05-03 17:04:44 +0400

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <div> Hello World!</div>;
+  return (
+    <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Navbar />
+    </div>
+  );
 }
---
+++ components/ui/SpotLight.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SpotLight = () => {
+  return <div>SpotLight</div>;
+};
